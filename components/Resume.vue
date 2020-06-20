145639959d828c62b2057b5095da25e0f285bad2
Ivan Greve <ivangreve@gmail.com>
2020-06-20 20:41:49 -0300

    Init commit

---
+++ components/Resume.vue
@@ -0,0 +1,54 @@
+<template>
+  <section id="page-top">
+    <div class="container-fluid p-0">
+      <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <Presentation />
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="experience">
+        <Experience />
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="skills">
+        <Skills />
+      </section>
+
+      <!-- <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="projects">
+        <Projects />
+      </section> -->
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="education">
+        <Education />
+      </section>
+
+      <section class="resume-section p-3 p-lg-5 d-flex flex-column" id="interests">
+        <Interests />
+      </section>
+    </div>
+  </section>
+</template>
+
+<script>
+import jump from "jump.js";
+import Presentation from "../components/Sections/Presentation";
+import Education from "../components/Sections/Education";
+import Experience from "../components/Sections/Experience";
+import Skills from "../components/Sections/Skills";
+import Interests from "../components/Sections/Interests";
+// import Projects from "../components/Sections/Projects";
+
+export default {
+  components: {
+    Presentation,
+    Experience,
+    Education,
+    Skills,
+    Interests,
+    // Projects
+  },
+  methods: {}
+};
+</script>
+
+<style>
+</style>
---
+++ components/Sections/Presentation.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="my-auto mx-auto">
+    <span class="d-block d-lg-none text-center mb-5">
+      <img
+        class="img-fluid img-profile rounded-circle mx-auto mb-2"
+        src="/profile.jpg"
+        alt=""
+      />
+    </span>
+
+    <h1 class="mb-2">
+      {{ $t('personalInfo.name') }}
+      <span class="text-primary">{{ $t('personalInfo.lastName') }}</span>
+    </h1>
+
+    <h3 class="mb-4">
+      <vue-typer
+        :text="$t('personalInfo.subHeadingText')"
+        :repeat="Infinity"
+        :shuffle="true"
+        initial-action="typing"
+        :pre-type-delay="100"
+        :type-delay="82"
+        :pre-erase-delay="2000"
+        :erase-delay="30"
+        erase-style="backspace"
+        :erase-on-complete="false"
+        caret-animation="blink"
+      ></vue-typer>
+    </h3>
+
+    <div class="subheading mb-5">
+      {{ $t('personalInfo.city') }} · {{ $t('personalInfo.phoneNumber') }} ·
+      <a href="mailto:name@email.com">{{ $t('personalInfo.email') }}</a>
+    </div>
+    <p class="mb-5">
+      {{ $t('personalInfo.myDescription') }}
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
